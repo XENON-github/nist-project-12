4beee8164bed214e53e508f7f38fddebb8cf353b
Sattyam_Thapaliya <sattyamthapaliya@gmail.com>
2025-12-25 14:17:17 +0545

    Update db.php

--- nss-project-12/db.php
+++ nss-project-12/db.php
@@ -2,10 +2,10 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "sql112.infinityfree.com";
-$username = "if0_40049058";       // MySQL user
-$password = "EgyptianMummy";      // MySQL password
-$dbname = "if0_40049058_library"; // Make sure it has your username as prefix!
+$servername = "";
+$username = "";       // MySQL user
+$password = "";      // MySQL password
+$dbname = ""; // Make sure it has your username as prefix!
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
